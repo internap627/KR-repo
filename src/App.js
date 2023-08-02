5aaca322b9220db039ba66edeeb5449860369994
github username <internap627@gmail.com>
2023-08-02 17:33:48 -0400

    Main footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Navbar';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  
+  return (
+    <footer style={{ backgroundColor: '#1e4f50', color: 'white', padding: '20px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <div>
+        {/* © and the year */}
+        &copy; {currentYear} Kwasi J Robinson
+      </div>
+      <div className='socials'>
+        {/* Social media icons */}
+        <a href="https://www.facebook.com/kwasirobinsonTPRC?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
+          <img src="fb-logo.png" alt="Facebook" />
+        </a>
+        <a href="https://twitter.com/KwasiJRobinson?t=JV0kPPWD7wDX6FulAl6aOQ&s=08" target="_blank" rel="noopener noreferrer">
+          <img src="twitter-logo.png" alt="Twitter" />
+        </a>
+        <a href="https://instagram.com/kwasijrobinson?igshid=MmU2YjMzNjRlOQ==" target="_blank" rel="noopener noreferrer">
+          <img src="insta-logo.png" alt="Instagram" />
+        </a>
+        {/* Add more social media links as needed */}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
