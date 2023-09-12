ccfd967677cc2a20fd902b772cadba826f952749
github username <internap627@gmail.com>
2023-09-12 15:18:47 -0400

    footer anchor

--- src/Footer.js
+++ src/Footer.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  
+
   return (
-    <footer style={{ backgroundColor: '#1e4f50', color: 'white', padding: '20px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    <footer
+      style={{
+        backgroundColor: "#1e4f50",
+        color: "white",
+        padding: "20px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 0,
+      }}
+    >
       <div>
         {/* © and the year */}
         &copy; {currentYear} Kwasi J Robinson
       </div>
-      <div className='socials'>
+      <div className="socials">
         {/* Social media icons */}
-        <a href="https://www.facebook.com/kwasirobinsonTPRC?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.facebook.com/kwasirobinsonTPRC?mibextid=ZbWKwL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="fb-logo.png" alt="Facebook" />
         </a>
-        <a href="https://twitter.com/KwasiJRobinson?t=JV0kPPWD7wDX6FulAl6aOQ&s=08" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://twitter.com/KwasiJRobinson?t=JV0kPPWD7wDX6FulAl6aOQ&s=08"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="twitter-logo.png" alt="Twitter" />
         </a>
-        <a href="https://instagram.com/kwasijrobinson?igshid=MmU2YjMzNjRlOQ==" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://instagram.com/kwasijrobinson?igshid=MmU2YjMzNjRlOQ=="
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="insta-logo.png" alt="Instagram" />
         </a>
         {/* Add more social media links as needed */}
